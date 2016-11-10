d31fcb31aa0e4ca9f5361ab2571b67b7ea7a6197
Kevin Wortman <kwortman@gmail.com>
2016-11-10 12:22:11 -0800

    skeleton code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# project3
-Project 3 - "Binary Search Tree"
+CSUF CPSC 131, Fall 2016
+Project 3
+"Binary Search Tree" (using the data structures learned in CPSC 131)
+
+Group members:
+
+Ada Lovelace adalovelace@csu.fullerton.edu
+
+Charles Babbage charlesbab@csu.fullerton.edu
+
+
+## You need to answer the following question. 
+Two binary search trees contains exactly the same number of entries of student information but in different orders.
+Do they give the same performance in terms of key searching or firstname searching? if not, why? use the data you obtain from you code to 
+explain your answer. 
---
+++ utilities.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include "SearchTree.h"
+
+
+SearchTree<Entry<string, string> >* buildBST_fromDatafile(const std::string& filename) {
+	std::ifstream f(filename.c_str());
+	if (!f)
+		throw std::runtime_error("cannot open data file");
+
+	std::string s;
+
+	getline(f, s);
+	std::string magic("CPSC131 Project3 raw data file");
+	if (!f || !std::equal(magic.begin(), magic.end(), s.begin()))
+		throw std::runtime_error("incorrect data file");
+	getline(f, s);
+
+	std::string firstName, movie;
+	
+	SearchTree<Entry<string, string>> *bst = new SearchTree<Entry<string, string>>;
+
+	// TO DO: implement the creation of a binary search tree
+
+
+	f.close();
+
+	return bst;
+}
+
+// TO DO: You need to implement a function to print a singly linked list in reverse order
+void printInReverseOder(SNode<Entry<string,string> >* node)
+{
+
+
+}
+
+
